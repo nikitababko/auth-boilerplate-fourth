9d9c0ef1cdd1e0426abba3354579396b82b25c85
Nikita Babko <niki-babko@yandex.ru>
2021-03-21 14:59:53 +0700

    Added routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Login, Register } from './components/Auth';
 import { Landing, Navbar } from './components/layout';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
