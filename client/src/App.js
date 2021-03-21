7d57769966cdf0bca41176260ea91e866ee65bf2
Nikita Babko <niki-babko@yandex.ru>
2021-03-21 18:22:58 +0700

    Added decode for token.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -8,6 +9,30 @@ import store from './redux/store';
 // My components
 import { Login, Register } from './components/Auth';
 import { Landing, Navbar } from './components/layout';
+import { Dashboard, PrivateRoute } from 'components';
+import setAuthToken from 'utils/setAuthToken';
+import { setCurrentUser, logoutUser } from 'redux/actions/authActions';
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwtDecode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = './login';
+  }
+}
 
 class App extends Component {
   render() {
@@ -19,6 +44,9 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
           </div>
         </Router>
       </Provider>
